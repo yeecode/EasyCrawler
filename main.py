07b0d653b9631295cc1fecc54f5897801e0ac8a5
yeecode <yeecode@126.com>
2020-05-30 09:46:45 +0800

    将代码语法从python2升级到python3

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import urllib
+import urllib.request, urllib.parse, urllib.error
 import re
 import socket
-import urlparse
+import urllib.parse
 
 def downloadWeb(url,pageId):
-    if urlMap.has_key(url):
+    if url in urlMap:
         pass
     else:
         pageId += 1
         urlMap[url]=pageId
         idMap[pageId]=url
-        htmlFile=open('./output/'+(str(pageId)+'.txt'),'w')
-        htmlFile.write(urllib.urlopen(url).read())
+        htmlFile=open('./output/'+(str(pageId)+'.txt'),'wb')
+        htmlFile.write(urllib.request.urlopen(url).read())
         htmlFile.close()
         
         htmlFile=open('./output/'+(str(pageId)+'.txt'),'r')
@@ -19,17 +19,17 @@ def downloadWeb(url,pageId):
             ans=re.findall(pattern,line)
             for one in ans :
                 urlTail=one.split('"')[1]            
-                url=urlparse.urljoin(url,urlTail)
-                if urlMap.has_key(url):
-                    print 'skip---'+url
+                url=urllib.parse.urljoin(url,urlTail)
+                if url in urlMap:
+                    print('skip---'+url)
                 else:
-                    print 'download---'+url
+                    print('download---'+url)
                     pageId += 1
                     urlMap[url]=pageId
                     idMap[pageId]=url
-                    catchFile=open('./output/'+(str(urlMap[url])+'.txt'),'w')
+                    catchFile=open('./output/'+(str(urlMap[url])+'.txt'),'wb')
                     try:
-                        catchFile.write(urllib.urlopen(url).read())
+                        catchFile.write(urllib.request.urlopen(url).read())
                     except:
                         pass
                     finally:
@@ -37,11 +37,11 @@ def downloadWeb(url,pageId):
         htmlFile.close()
 
     urlMapFile=open('./output/map.txt','w')
-    for key in idMap.keys():
+    for key in list(idMap.keys()):
         urlMapFile.write(str(key)+'\t'+str(idMap[key])+'\n')
     urlMapFile.close()
     
-    print 'success!'
+    print('success!')
 
 
 if __name__=='__main__':
